1c9784e8daf726566b375a6e9c42accbd94eb275
Cody Cuellar <cody.cuellar@gmail.com>
2017-12-19 15:17:29 -0800

    Updated docstrings

--- ultrasequence/sequencer.py
+++ ultrasequence/sequencer.py
@@ -9,11 +9,12 @@ DEFAULT_FORMAT = '%H%r%T'
 def extract_frame(name):
 	"""
 	This function extracts the last set of digits in the string name and
-	assumes it is the frame number when attempting to match with other file
-	names. This function should be passed basenames only, so it doesn't
-	attempt to sequence directory names.
+	assumes it is the frame number when returning the parts.
+	
+	It's a good idea to only pass basenames without extenions so it doesn't
+	attempt to sequence directory names or digits in the extension.
 
-	:param str name: file basename without dir  
+	:param str name: file basename without dir or extension
 	:return: 3-pair tuple consisting of the head (all characters preceding the
 			 last set of digits, the frame number (last set of digits), and
 			 tail (all digits succeeding the frame number).
@@ -32,7 +33,7 @@ def extract_frame(name):
 def split_extension(filename):
 	"""
 	Splits the extension off the filename and returns a tuple of the 
-	base filename and the extension.
+	base filename and the extension (without the dot).
 	"""
 	parts = filename.split('.')
 	if len(parts) < 2:
@@ -43,6 +44,13 @@ def split_extension(filename):
 
 
 def frame_ranges_to_string(frame_list):
+	"""
+	Take a flat list of ordered numbers and make a string representation
+	of the ranges.
+	
+	:param iterable frame_list: 
+	:return: string of broken frame ranges (i.e '[10-14, 16, 20-25]')
+	"""
 	ranges = [[frame_list.pop(0)]]
 	range_i = 0
 	for x in frame_list:
@@ -96,19 +104,21 @@ class File(object):
 		"""
 		Class which represents single files or frames on disk.
 		While initializing this object, it can be fed stat values
-		directly or can attempt to call them on the fly with
-		get_stats.
+		directly or can attempt to call them on the fly by setting
+		get_stats to True.
 		
 		When passing data into the stat argument, the object passed
 		in can be either an actual os.stat_result object, a dictionary
-		mapping that matches the sequencer.Stat paramater names, or an
+		mapping that matches the sequencer.Stat parameter names, or an
 		iterable of int like items that matches the order of the 
-		sequencer.Stat class params.
+		sequencer.Stat class params in the __init__ method.
 		
 		:param str filepath: the absolute filepath of the file
 		:param stats: dict or iterable to map to sequencer.Stat params
 			or os.stat_result object.
-		:param bool get_stats: when True, 
+		:param bool get_stats: when True, attempt to call os.stat on the file.
+			If file does not exists, revert back to applying stats values
+			if they were supplied.
 		"""
 		self.abspath = filepath
 		self.path, self.name = os.path.split(filepath)
@@ -149,6 +159,7 @@ class File(object):
 
 	@property
 	def frame(self):
+		""" Integer frame number """
 		try:
 			return int(self._framenum)
 		except ValueError:
@@ -156,10 +167,12 @@ class File(object):
 
 	@property
 	def frame_as_str(self):
+		""" String frame number extracted from original filename """
 		return self._framenum
 
 	@property
 	def size(self):
+		""" Stat size of file on disk. None if stat not run or supplied. """
 		try:
 			return int(self.stat.st_size)
 		except TypeError:
@@ -167,6 +180,7 @@ class File(object):
 
 	@property
 	def inode(self):
+		""" Stat inode of file on disk. None if stat not run or supplied. """
 		try:
 			return int(self.stat.st_ino)
 		except TypeError:
@@ -174,6 +188,7 @@ class File(object):
 
 	@property
 	def nlink(self):
+		""" Stat nlink of file on disk. None if stat not run or supplied. """
 		try:
 			return int(self.stat.st_nlink)
 		except TypeError:
@@ -181,6 +196,7 @@ class File(object):
 
 	@property
 	def dev(self):
+		""" Stat dev of file on disk. None if stat not run or supplied. """
 		try:
 			return int(self.stat.st_dev)
 		except TypeError:
@@ -188,6 +204,7 @@ class File(object):
 
 	@property
 	def mode(self):
+		""" Stat mode of file on disk. None if stat not run or supplied. """
 		try:
 			return int(self.stat.st_mode)
 		except TypeError:
@@ -195,6 +212,7 @@ class File(object):
 
 	@property
 	def uid(self):
+		""" Stat uid of file on disk. None if stat not run or supplied. """
 		try:
 			return int(self.stat.st_uid)
 		except TypeError:
@@ -202,6 +220,7 @@ class File(object):
 
 	@property
 	def gid(self):
+		""" Stat gid of file on disk. None if stat not run or supplied. """
 		try:
 			return int(self.stat.st_gid)
 		except TypeError:
@@ -209,6 +228,7 @@ class File(object):
 
 	@property
 	def ctime(self):
+		""" Stat ctime of file on disk. None if stat not run or supplied. """
 		try:
 			return float(self.stat.st_ctime)
 		except TypeError:
@@ -216,6 +236,7 @@ class File(object):
 
 	@property
 	def mtime(self):
+		""" Stat mtime of file on disk. None if stat not run or supplied. """
 		try:
 			return float(self.stat.st_mtime)
 		except TypeError:
@@ -223,6 +244,7 @@ class File(object):
 
 	@property
 	def atime(self):
+		""" Stat atime of file on disk. None if stat not run or supplied. """
 		try:
 			return float(self.stat.st_atime)
 		except TypeError:
@@ -232,7 +254,7 @@ class File(object):
 		"""
 		Make sequence name identifier
 		
-		:param bool padding: enforce padding 
+		:param bool ignore_padding: enforce padding 
 		:return: sequence name with '#' for frame number if padding ignored
 			or standerd padding format '%0#d' where '#' is padding amount. 
 		"""
@@ -248,8 +270,8 @@ class File(object):
 class Sequence(object):
 	def __init__(self, file=None, ignore_padding=True):
 		"""
-		Class representing a sequence of matching filenames. The frames
-		are stored in a dicitonary with the frame numbers as keys. Sets
+		Class representing a sequence of matching file names. The frames
+		are stored in a dictionary with the frame numbers as keys. Sets
 		are used for fast operations in calculating missing frames.
 		
 		:param file: File object or filename string to base the object
@@ -293,46 +315,55 @@ class Sequence(object):
 
 	@property
 	def start(self):
+		""" First frame of sequence """
 		return min(self._frames)
 
 	@property
 	def end(self):
+		""" Last frame of sequence """
 		return max(self._frames)
 
 	@property
 	def frames(self):
+		""" Number of total frames actually in sequence """
 		return len(self)
 
 	@property
 	def implied_frames(self):
+		""" Number of expected frames in sequence """
 		return self.end - self.start + 1
 
 	@property
 	def missing_frames(self):
+		""" Number of missing frames in sequence """
 		return self.end - (self.start - 1) - self.frames
 
 	@property
 	def is_missing_frames(self):
+		""" True if non-contiguous portions of the frame numbers """
 		return self.frames != self.implied_frames
 
 	@property
 	def size(self):
+		""" Sum of all filesizes in sequence if available """
 		try:
 			return sum([file.size for file in self])
 		except TypeError:
 			return
 
 	def get_frames(self):
+		""" Get a list of all frame numbers actually existing in sequence """
 		return sorted(list(self._frames))
 
 	def get_missing_frames(self):
+		""" Get list of frame numbers missing between start and end frame """
 		implied = set(range(self.start, self.end + 1))
 		actual = set(self._frames)
 		return list(sorted(implied - actual))
 
 	def append(self, file):
 		"""
-		Append a new frame to the sequence.
+		Add a new frame to the sequence.
 		 
 		:param file: File object or string to append to Sequence
 		"""
@@ -427,43 +458,56 @@ class Sequence(object):
 		return format
 
 	def __path(self):
+		""" Internal formatter method """
 		return self.path
 
 	def __namehead(self):
+		""" Internal formatter method """
 		return self.namehead
 
 	def __head(self):
+		""" Internal formatter method """
 		return self.head
 
 	def __num_frames(self):
+		""" Internal formatter method """
 		return str(self.frames)
 
 	def __short_range(self):
+		""" Internal formatter method """
 		return '[' + str(self[self.start].frame_as_str) + \
 			   '-' + str(self[self.end].frame_as_str) + ']'
 
 	def __explicit_range(self):
+		""" Internal formatter method """
 		return frame_ranges_to_string(self.get_frames())
 
 	def __num_missing_frames(self):
+		""" Internal formatter method """
 		return str(self.missing_frames)
 
 	def __explicit_missing_range(self):
+		""" Internal formatter method """
 		return frame_ranges_to_string(self.get_missing_frames())
 
 	def __pound_padding(self):
+		""" Internal formatter method """
 		return '#' * self.padding
 
 	def __formatted_padding(self):
+		""" Internal formatter method """
 		return '%%0%dd' % self.padding
 
 	def __tail_without_ext(self):
+		""" Internal formatter method """
 		return '.'.join('.'.split(self.tail)[:-1])
 
 	def __tail(self):
+		""" Internal formatter method """
 		return self.tail
 
 	def __ext(self):
+		""" Internal formatter method """
 		return self.ext
 
 
