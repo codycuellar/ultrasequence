d308cefd2cbb883159f9c39ed2196308ddb7edfb
Cody Cuellar <cody.cuellar@gmail.com>
2017-12-31 14:32:27 -0800

    added Stat to __init__

--- ultrasequence/__init__.py
+++ ultrasequence/__init__.py
@@ -1,5 +1,5 @@
 from .version import __version__
-from .models import File, Sequence
+from .models import File, Sequence, Stat
 from .parsing import Parser
 
-__all__ = [File, Sequence, Parser]
+__all__ = [File, Sequence, Parser, Stat]
