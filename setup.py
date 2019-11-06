d5151fd4e156ec5ffd1a7657c9f6f0af0f5107b2
joss13 <joss13@gmail.com>
2019-11-05 22:18:46 -0800

    readme.rst -> README.rst in setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
 	url=URL,
 	license=LICENSE,
 	description=DESCRIPTION,
-	long_description=open('readme.rst').read(),
+	long_description=open('README.rst').read(),
 	keywords='sequence file parser image ultra frames',
 	platforms=['MacOS 10.10', 'MacOS 10.11', 'MacOS 10.12', 'MacOS 10.13'],
 	python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
